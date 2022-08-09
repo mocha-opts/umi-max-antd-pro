fc1460303235d2f396ac8d3d21c792bf3e9562d6
huangyihai <huangyihai@huayun.com>
2022-08-09 18:02:51 +0800

    chore: init project

---
+++ .umirc.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  locale: {
+    // 默认使用 src/locales/zh-CN.ts 作为多语言文件
+    default: 'zh-CN',
+    baseSeparator: '-',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: '首页',
+      path: '/home',
+      component: './Home',
+    },
+    {
+      name: '权限演示',
+      path: '/access',
+      component: './Access',
+    },
+    {
+        name: ' CRUD 示例',
+        path: '/table',
+        component: './Table',
+    },
+  ],
+  npmClient: 'pnpm',
+});
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# umi-max-antd-pro
-create by umi max
+# README
+
+`@umijs/max` 模板项目，更多功能参考 [Umi Max 简介](https://next.umijs.org/zh-CN/docs/max/introduce)
